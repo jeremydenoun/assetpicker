b23e2f283c130e5c6eb39c448bbd9b12cc0fd7d0
Jeremy Denoun <jeremy.denoun@iguanesolutions.com>
2017-04-20 22:44:02 +0200

    xerox basE

---
+++ src/js/adapter/xerox_scanner/index.js
@@ -0,0 +1,64 @@
+module.exports = {
+    translations: {
+        description: {
+            en: 'Xerox Scanner',
+            de: 'Xerox Scanner',
+            fr: 'Xerox Scanner'
+        }
+    },
+    http: {
+        base: 'http://printer-01',
+        list: '/mailbox/folder.php?name=_PUBLIC',
+        download: '/mailbox/action.php?action=frc_dwnld&name=_PUBLIC&file='
+    },
+    events: {
+        'load-items': function (tree) {
+            if (this.lastId >= 1400) {
+                return;
+            }
+            this.mockLoad(function() {
+                tree.items = this.createItems();
+            });
+        },
+        'load-more-items': function (items) {
+            if (this.lastId >= 1400) {
+                return;
+            }
+            this.mockLoad(function() {
+                this.createItems().forEach(function(item) {
+                    items.push(item);
+                });
+            });
+        }
+    },
+    methods: {
+        mockLoad: function(callback) {
+            this.$root.loading++;
+            window.setTimeout(function() {
+                callback.call(this);
+                this.$root.loading--;
+            }.bind(this), 500);
+        },
+        item: function(extension, thumbnail) {
+            return this.createItem({
+                id: '' + (this.lastId++),
+                type: extension ? 'file' : 'dir',
+                extension: extension,
+                name: 'Random ' + (extension || ' directory') + (thumbnail ? ' with thumb' : '') + ' ' + this.lastId,
+                thumbnail: thumbnail
+            });
+        },
+        createItems: function () {
+            var items = [],
+                extensions = ['txt', 'pdf', 'xls', 'doc', 'pot', 'jpeg', 'zip', 'mp3', 'avi', 'html', 'any'];
+            items.push(this.item());
+            for (var i = 0, l = extensions.length; i < l; i++) {
+                items.push(this.item(extensions[i]));
+            }
+            items.push(this.item('jpeg', 'http://lorempixel.com/nature/160/200'));
+            items.push(this.item('jpeg', 'http://lorempixel.com/nature/200/160'));
+            items.total = 10 * items.length;
+            return items;
+        }
+    }
+};
