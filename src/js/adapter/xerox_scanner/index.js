27b0b023e4fc7d3cc912c807b27ecb9ddcebc1f8
Jeremy Denoun <jeremy.denoun@iguanesolutions.com>
2017-05-19 16:53:00 +0200

    fix

--- src/js/adapter/xerox_scanner/index.js
+++ src/js/adapter/xerox_scanner/index.js
@@ -29,43 +29,33 @@ module.exports = {
         }
     },
     methods: {
-        item: function(extension, thumbnail) {
-            return this.createItem({
-                id: '' + (this.lastId++),
-                type: extension ? 'file' : 'dir',
-                extension: extension,
-                name: 'Random ' + (extension || ' directory') + (thumbnail ? ' with thumb' : '') + ' ' + this.lastId,
-                thumbnail: thumbnail
-            });
-        },
         loadAssets: function () {
             var result = {page: 0, pages: 0, items: []};
+            console.log(this);
             this.http.get(
                 'mailbox/folder.php?name=' + this.config.directory
             ).then((function(response) {
                 var data = JSON.parse(response.data);
                 response.data = data;
-		console.log(response.data);
                 result.page = parseInt(response.data.page);
                 result.pages = parseInt(response.data.pages);
                 var downloadUrl = this.config.url.replace(/\/+$/, '') + '/mailbox/action.php?action=frc_dwnld&name=' + this.config.directory + "&file=";
-		if (response.data && response.data.results) {
-		  console.log("iterate");
-                  response.data.results.forEach((function (asset) {
-                    var item = this.createItem({
-                      id: asset.id,
-                      type: asset.isfolder ? 'dir' : 'file',
-                      name: asset.name,
-                      extension: asset.extension,
-                      links: {
-                        download: downloadUrl + asset.id,
-                        open: downloadUrl + asset.id,
-                      },
-                      data: asset
-                    });
-                    result.items.push(item);
-                  }).bind(this));
-	       }
+        		if (response.data && response.data.results) {
+                    response.data.results.forEach((function (asset) {
+                        var item = this.createItem({
+                          id: asset.id,
+                          type: asset.isfolder ? 'dir' : 'file',
+                          name: asset.name,
+                          extension: asset.extension,
+                          links: {
+                            download: downloadUrl + asset.id,
+                            open: downloadUrl + asset.id,
+                          },
+                          data: asset
+                        });
+                        result.items.push(item);
+                    }).bind(this));
+        	   }
             }).bind(this));
             return result.items;
         }
